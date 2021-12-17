a27c21d3a583cef8358c20ee97b7fa54799a2661
keithwillis-bc <kwillis@beckman.com>
2021-12-17 07:47:17 -0500

    added authentication

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
+const session = require("express-session");
+const mongoDbStore = require("connect-mongodb-session");
 
-const db = require('./data/database');
-const demoRoutes = require('./routes/demo');
+const db = require("./data/database");
+const demoRoutes = require("./routes/demo");
+const { Http2ServerRequest } = require("http2");
+
+const MongoDbStore = mongoDbStore(session);
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const sessionStore = new MongoDbStore({
+  uri: "mongodb://localhost:27017",
+  databaseName: "auth-demo",
+  collection: "sessions",
+});
 
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      maxAge: 60 * 1000 * 20,
+    },
+  })
+);
 
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
--- routes/demo.js
+++ routes/demo.js
@@ -1,29 +1,108 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcryptjs");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-router.get('/signup', function (req, res) {
-  res.render('signup');
+router.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
-router.get('/login', function (req, res) {
-  res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.post('/signup', async function (req, res) {});
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData["confirm-email"];
+  const enteredPassword = userData.password;
 
-router.post('/login', async function (req, res) {});
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
 
-router.get('/admin', function (req, res) {
-  res.render('admin');
+  if (
+    enteredEmail !== enteredConfirmEmail ||
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredPassword.trim().length < 6 ||
+    !enteredEmail.includes("@")
+  ) {
+    console.log("incorrect data");
+    return res.redirect("/signup");
+  }
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (existingUser) {
+    console.log("duplicate user");
+    return res.redirect("/signup");
+  }
+
+  const user = {
+    email: enteredEmail.toLowerCase(),
+    password: hashedPassword,
+  };
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
 });
 
-router.post('/logout', function (req, res) {});
+router.post("/login", async function (req, res) {
+  const enteredPassword = req.body.password;
+  const enteredEmail = req.body.email;
+  const user = {
+    email: enteredEmail.toLowerCase(),
+  };
+
+  const loggedInUser = await db.getDb().collection("users").findOne(user);
+
+  if (!loggedInUser) {
+    //send an error back
+    console.log("could not log in");
+    return res.redirect("/login");
+  }
+
+  const passwordsMatch = await bcrypt.compare(
+    enteredPassword,
+    loggedInUser.password
+  );
+  if (!passwordsMatch) {
+    console.log("could not log in, passwords do not match");
+    return res.redirect("/login");
+  }
+
+  console.log("user is authenticated");
+
+  req.session.user = { id: loggedInUser._id, email: loggedInUser.email };
+  req.session.isAuthenticated = true;
+  req.session.save(() => {
+    res.redirect("/admin");
+  });
+});
+
+router.get("/admin", function (req, res) {
+  if (!req.session.isAuthenticated) {
+    return res.status(401).render("401");
+  }
+
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {
+  req.session.user = null;
+  req.session.isAuthenticated = false;
+  req.session.save(() => {
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
